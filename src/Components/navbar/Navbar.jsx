e5c3a3bc4df8f6d47b2f47d9c22eec6b8f41dabf
rama.fall <ramafall49@gmail.com>
2023-11-14 17:39:38 +0000

    w

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -31,16 +31,16 @@ export default function Navbar() {
                     { label: < img src={Imag} alt='Imag' style={logo}/>, key: "logo", },
                     { label: <Select defaultValue="QUI SOMMES NOUS?" onChange={handleChange}>
                    
-                    <Option value="QUI SOMMES NOUS?" >QUI SOMMES NOUS?</Option>
-                    <Option value="QUI SOMMES NOUS?" >QUI SOMMES NOUS?</Option>
-                </Select>, key: "QUI SOMMES NOUS?" },
+                    <Option value="Qui sommes nous?" >Qui sommes nous?</Option>
+                    <Option value="Qui sommes nous?" >Qui sommes nous?</Option>
+                </Select>, key: "Qui sommes nous?" },
                     { label: "Qui sommes nous?", key: "Qui sommes nous?" },
                     { label: "Programmes", key: "Programmes" },
-                    { label: <Select defaultValue="QUI SOMMES NOUS?" onChange={handleChange}>
+                    { label: <Select defaultValue="Qui sommes nous?" onChange={handleChange}>
                    
-                    <Option value="DEMANDES" >QUI SOMMES NOUS?</Option>
-                    <Option value="DEMANDES" >QUI SOMMES NOUS?</Option>
-                </Select>, key: "DEMANDES" },
+                    <Option value="Demandes" >Demandes</Option>
+                    <Option value="Demandes" >Demandes</Option>
+                </Select>, key: "Demandes" },
                     { label: "Galerie", key: "Galerie" },
                     { label: "Contactez-nous", key: "Contactez-nous" },
                     { label: "Faire un don", key: "Faire un don" },
